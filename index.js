aa810004aa03df96bfed45a2b6d7bd70140bd5ab
Santhosh kuamr <santhoshfsdev@gmail.com>
2020-04-04 21:43:46 -0400

    server editing

--- index.js
+++ index.js
@@ -3,10 +3,5 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.listen(port);
