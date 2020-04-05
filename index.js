71e5ecd477199498d6f4f1f2ca2836ffc03b1665
Santhosh kuamr <santhoshfsdev@gmail.com>
2020-04-04 21:53:55 -0400

    added path

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static('BetterthanB4/build'))
+
+    app.get('/',(req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'BetterthanB4' , 'build', 'index.html'))
+    })
+}
+
 
 app.listen(port);
